2249c62e80c855801c5b57e27001dde557d46ab1
Ashot Abazyan <ashot.93@mail.ru>
2022-06-28 01:15:30 +0300

    27/06

--- password_check.py
+++ password_check.py
@@ -1,70 +1,54 @@
 import urwid
 
 
-def is_very_long():
-    if len(str(ask)) < 12:
-        return False
-    return True
+def is_very_long(password):
+    return len(str(password)) > 12
 
 
-def has_digit():
-    for element in str(ask):
-        if element.isdigit():
-            return True
-    return False
+def has_digit(password):
+    return any(element.isdigit() for element in str(password))
 
 
-def has_letters():
-    for element in str(ask):
-        if element.isalpha():
-            return True
-    return False
+def has_letters(password):
+    return any(element.isalpha() for element in str(password))
 
 
-def has_upper_letters():
-    for element in str(ask):
-        if element.isupper():
-            return True
-    return False
+def has_upper_letters(password):
+    return any(element.isupper() for element in str(password))
 
 
-def has_lower_letters():
-    for element in str(ask):
-        if element.islower():
-            return True
-    return False
+def has_lower_letters(password):
+    return any(element.islower() for element in str(password))
 
 
-def has_symbols():
-    for element in str(ask):
-        if element.isdigit() and element.isalpha():
-            return False
-    return True
+def has_symbols(password):
+    return any(not element.isalnum() for element in str(password))
 
 
-def rating_score():
-    score = 0
-    score_count = [
-        is_very_long(),
-        has_digit(),
-        has_letters(),
-        has_upper_letters(),
-        has_lower_letters(),
-        has_symbols()
+def rating_score(password):
+    validators = [
+        is_very_long,
+        has_digit,
+        has_letters,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols
     ]
-    for el in score_count:
-        if el is True:
+    score = 0
+    for validator in validators:
+        if validator(password):
             score += 2
     return score
 
 
-def on_ask_change(edit, new_edit_text):
-    reply.set_text("Рейтинг пароля: %s" % rating_score())
+def password_rait(edit, new_edit_text):
+    reply.set_text("Рейтинг пароля: %s" % rating_score(new_edit_text))
 
 
-ask = urwid.Edit('Пароль: ', mask='*')
-reply = urwid.Text("")
-menu = urwid.Pile([ask, reply])
-menu = urwid.Filler(menu, valign='top')
-urwid.connect_signal(ask, 'change', on_ask_change)
-urwid.MainLoop(menu).run()
+if __name__ == '__main__':
+    password = urwid.Edit('Пароль: ', mask='*')
+    reply = urwid.Text("")
+    menu = urwid.Pile([password, reply])
+    menu = urwid.Filler(menu, valign='top')
+    urwid.connect_signal(password, 'change', password_rait)
+    urwid.MainLoop(menu).run()
